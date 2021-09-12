d86bf722ae2063039c5dbda3ef20ba48fac3a4eb
Fernando Jascovich <fernando.ej@gmail.com>
2021-09-11 22:46:25 -0300

    minidox update

--- keyboards/minidox/keymaps/fernando_jascovich/keymap.c
+++ keyboards/minidox/keymaps/fernando_jascovich/keymap.c
@@ -1,159 +1,61 @@
-#include "keymap.h"
-#include "tapdance.c"
+#include QMK_KEYBOARD_H
+
+#define _QWERTY 0
+#define _DVORAK 1
+#define _LOWER  3
+#define _RAISE  4
+#define _VIM    7
+#define _MOUSE  10
+#define _ADJUST 16
+
+#define _______ KC_TRNS
+#define XXXXXXX KC_NO
+#define KC_A1   MO(_LOWER)
+#define KC_A2   LALT_T(KC_TAB)
+#define KC_A3   KC_LCTRL
+#define KC_A4   KC_SPACE
+#define KC_A5   LGUI_T(KC_ENTER)
+#define KC_A6   MO(_RAISE)
 
 extern keymap_config_t keymap_config;
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-/* Qwerty
- * ,----------------------------------.           ,----------------------------------.
- * |   Q  |   W  |   E  |   R  |   T  |           |   Y  |   U  |   I  |   O  |   P  |
- * |------+------+------+------+------|           |------+------+------+------+------|
- * |   A  |   S  |   D  |   F  |   G  |           |   H  |   J  |   K  |   L  |TD_SFT|
- * |------+------+------+------+------|           |------+------+------+------+------|
- * |   Z  |   X  |   C  |   V  |   B  |           |   N  |   M  |   ,  |   .  | /|AGR|
- * `----------------------------------'           `----------------------------------'
- *                  ,--------------------.    ,------,-------------.
- *                  |LOWER |EMACS | Ctrl |    |      |RET/GU| RAISE|
- *                  `-------------| ---- |    | Space|------+------.
- *                                | LAlt |    |      |
- *                                `------'    `------'
- */
   [_QWERTY] = LAYOUT( \
-    KC_Q,  KC_W,       KC_E,             KC_R,     KC_T,              KC_Y, KC_U, KC_I,    KC_O,   KC_P, \
-    KC_A,  KC_S,       KC_D,             KC_F,     KC_G,              KC_H, KC_J, KC_K,    KC_L,   TD(TD_SHIFT), \
-    KC_Z,  KC_X,       KC_C,             KC_V,     KC_B,              KC_N, KC_M, KC_COMM, KC_DOT, ALGR_T(KC_SLASH), \
-    LOWER, MO(_EMACS), TD(TD_CTRL_LALT), KC_SPACE, LGUI_T(KC_ENTER),  RAISE \
+          KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,  KC_Y, KC_U, KC_I,    KC_O,   KC_P,             \
+          KC_A,  KC_S,  KC_D,  KC_F,  KC_G,  KC_H, KC_J, KC_K,    KC_L,   RSFT_T(KC_BSPC),  \
+          KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,  KC_N, KC_M, KC_COMM, KC_DOT, ALGR_T(KC_SLASH), \
+          KC_A1, KC_A2, KC_A3, KC_A4, KC_A5, KC_A6 \
     ),
-
-/* Dvorak
- *
- * ,----------------------------------.           ,----------------------------------.
- * | /|AGR|   ,  |   .  |   P  |   Y  |           |   F  |   G  |   C  |   R  |   L  |
- * |------+------+------+------+------|           |------+------+------+------+------|
- * |   A  |   O  |   E  |   U  |   I  |           |   D  |   H  |   T  |   N  |   S  |
- * |------+------+------+------+------|           |------+------+------+------+------|
- * |TD_SFT|   Q  |   J  |   K  |   X  |           |   B  |   M  |   W  |   V  |   Z  |
- * `----------------------------------'           `----------------------------------'
- *                  ,--------------------.    ,------,-------------.
- *                  |LOWER |EMACS | Ctrl |    |      |RET/GU| RAISE|
- *                  `-------------| ---- |    | Space|------+------.
- *                                | LAlt |    |      |
- *                                `------'    `------'
- */
-[_DVORAK] = LAYOUT ( \
-  ALGR_T(KC_SLASH), KC_COMM,    KC_DOT,           KC_P,     KC_Y,              KC_F, KC_G, KC_C, KC_R, KC_L, \
-  KC_A,             KC_O,       KC_E,             KC_U,     KC_I,              KC_D, KC_H, KC_T, KC_N, KC_S, \
-  TD(TD_SHIFT),     KC_Q,       KC_J,             KC_K,     KC_X,              KC_B, KC_M, KC_W, KC_V, KC_Z, \
-  LOWER,            MO(_EMACS), TD(TD_CTRL_LALT), KC_SPACE, LGUI_T(KC_ENTER),  RAISE \
-),
-
-/* Lower
- * ,----------------------------------.           ,----------------------------------.
- * |   1  |   2  |   3  |   4  |   5  |           |   (  |   )  |   [  |   ]  |   `  |
- * |------+------+------+------+------|           |------+------+------+------+------|
- * |   6  |   7  |   8  |   9  |   0  |           | Left | Down |  Up  | Right|   |  |
- * |------+------+------+------+------|           |------+------+------+------+------|
- * |      |      |      |      |      |           |   {  |   }  |   <  |   >  |   \  |
- * `----------------------------------'           `----------------------------------'
- *                  ,--------------------.    ,------,-------------.
- *                  |LOWER |EMACS | Ctrl |    |      | Enter| RAISE|
- *                  `-------------| ---- |    |BckSpc|------+------.
- *                                | LAlt |    |      |
- *                                `------'    `------'
- */
-  [_LOWER] = LAYOUT(\
-    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_LPRN, KC_RPRN, KC_LBRC, KC_RBRC, KC_GRV, \
-    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_PIPE, \
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LCBR, KC_RCBR, KC_LT, KC_GT,  KC_BSLS, \
-    _______, _______, _______, KC_BSPC, _______, _______ \
-    ),
-
-/* Raise
- * ,----------------------------------.           ,----------------------------------.
- * |  Esc |   ^  |   *  |   +  |  %   |           |  ¡   |  ¿   |   @  |   $  |  Del |
- * |------+------+------+------+------|           |------+------+------+------+------|
- * |CapsLk|   ~  |   /  |   -  |  =   |           |  !   |  ?   |   #  |   &  |      |
- * |------+------+------+------+------|           |------+------+------+------+------|
- * |   :  |   ;  |   '  |   "  |  _   |           |      |      |      |      |Moutgl|
- * `----------------------------------'           `----------------------------------'
- *                  ,--------------------.    ,------,-------------.
- *                  |LOWER |EMACS |      |    |      | Enter| RAISE|
- *                  `-------------| Tab  |    | Space|------+------.
- *                                |      |    |      |
- *                                `------'    `------'
- */
-  [_RAISE] = LAYOUT( \
-    KC_ESC,  KC_CIRC, KC_ASTR,  KC_PLUS,  KC_PERC, ALGR(LSFT(KC_1)), ALGR(KC_SLSH), KC_AT,   KC_DOLLAR, KC_DEL,     \
-    KC_CAPS, KC_TILD, KC_SLASH, KC_MINUS, KC_EQL,  KC_EXLM,          KC_QUES,       KC_HASH, KC_AMPR,   XXXXXXX,    \
-    KC_COLN, KC_SCLN, KC_QUOT,  KC_DQUO,  KC_UNDS, XXXXXXX,          XXXXXXX,       XXXXXXX, XXXXXXX,   TG(_MOUSE), \
-    _______, _______, KC_TAB,   _______,  _______, _______\
-    ),
-
-/*Adjust(Lower+Raise)
- *
- * ,----------------------------------.           ,----------------------------------.
- * |  F1  |  F2  |  F3  |  F4  |  F5  |           |AudioT|Audio-|Audio+|  F14 |  F15 |
- * |------+------+------+------+------|           |------+------+------+------+------|
- * |  F6  |  F7  |  F8  |  F9  |  F10 |           | Home |PgDown| PgUp |  End |      |
- * |------+------+------+------+------|           |------+------+------+------+------|
- * |  F11 |  F12 |QWERTY|DVORAK|Reset |           |      |      |C+S+PS| S+PS |  PS  |
- * `----------------------------------'           `----------------------------------'
- *                  ,--------------------.    ,------,-------------.
- *                  |LOWER |EMACS | Ctrl |    |      | Enter| RAISE|
- *                  `-------------| ---- |    | Space|------+------.
- *                                | LAlt |    |      |
- *                                `------'    `------'
- */
-  [_ADJUST] =  LAYOUT( \
-    KC_F1,   KC_F2,   KC_F3,        KC_F4,       KC_F5,   KC__MUTE, KC__VOLDOWN, KC__VOLUP,           KC_F14,        KC_F15,  \
-    KC_F6,   KC_F7,   KC_F8,        KC_F9,       KC_F10,  KC_HOME,  KC_PGDN,     KC_PGUP,             KC_END,        XXXXXXX, \
-    KC_F11,  KC_F12,  DF(_QWERTY),  DF(_DVORAK), RESET,   XXXXXXX,  XXXXXXX,     LCTL(LSFT(KC_PSCR)), LSFT(KC_PSCR), KC_PSCR, \
-    _______, _______, _______,  _______, _______, _______\
-    ),
-
-/*Emacs
- *
- * ,----------------------------------.           ,----------------------------------.
- * |SpitNo|SplitH|SplitV| W+V  | W+H  |           |      |      |      |      |      |
- * |------+------+------+------+------|           |------+------+------+------+------|
- * |BufSel|BufOth|BufHid|BufKil|      |           |Commt |QueryR|RegexR|      |      |
- * |------+------+------+------+------|           |------+------+------+------+------|
- * |RegSet|RegJmp|      |OrgClI|OrgClO|           |MacroS|MacroE|  Top |Bottom|EM_ZAP|
- * `----------------------------------'           `----------------------------------'
- *                  ,--------------------.    ,------,-------------.
- *                  |LOWER |EMACS | Ctrl |    |      | Enter| RAISE|
- *                  `-------------| ---- |    | Space|------+------.
- *                                | LAlt |    |      |
- *                                `------'    `------'
- */
-  [_EMACS] =  LAYOUT( \
-    EM_SPLIT_NONE, EM_SPLIT_H,   EM_SPLIT_V,  EM_W_INC_V,  EM_W_INC_H,   XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,   XXXXXXX, \
-    EM_BUF_SELECT, EM_BUF_OTHER, EM_BUF_HIDE, EM_BUF_KILL, XXXXXXX,      EM_COMMENT, EM_QUERY_R, EM_REGEX_R, XXXXXXX,   XXXXXXX,    \
-    EM_REG_SET,    EM_REG_JUMP,  XXXXXXX,     EM_ORG_CL_I, EM_ORG_CL_O,  EM_MAC_ST,  EM_MAC_END, EM_TOP,     EM_BOTTOM, EM_ZAP,  \
-    _______,       _______,      _______,     _______,     _______,      _______\
-    ),
-
-/*Mouse
- *
- * ,----------------------------------.           ,----------------------------------.
- * |      |      |      |      |      |           |      |      |      |      |      |
- * |------+------+------+------+------|           |------+------+------+------+------|
- * |      |      |      |      |      |           |Mou L |Mou D |Mou T |Mou R |      |
- * |------+------+------+------+------|           |------+------+------+------+------|
- * |      |      |Mou 1 |Mou 2 |Mou 3 |           |Mou WL|Mou WD|Mou WT|Mou WR|MouTgl|
- * `----------------------------------'           `----------------------------------'
- *                  ,--------------------.    ,------,-------------.
- *                  |      |      |      |    |      |      | RAISE|
- *                  `-------------|      |    |      |------+------.
- *                                |      |    |      |
- *                                `------'    `------'
- */
+  [_DVORAK] = LAYOUT ( \
+          ALGR_T(KC_SLASH), KC_COMM, KC_DOT, KC_P,  KC_Y,  KC_F, KC_G, KC_C, KC_R, KC_L, \
+          KC_A,             KC_O,    KC_E,   KC_U,  KC_I,  KC_D, KC_H, KC_T, KC_N, KC_S, \
+          RSFT_T(KC_BSPC),  KC_Q,    KC_J,   KC_K,  KC_X,  KC_B, KC_M, KC_W, KC_V, KC_Z, \
+          KC_A1,            KC_A2,   KC_A3,  KC_A4, KC_A5, KC_A6 \
+          ),
+  [_LOWER] = LAYOUT(
+          KC_1,    KC_2,    KC_3,    KC_0,         XXXXXXX, KC_LPRN, KC_RPRN, KC_AT,         KC_HASH, KC_DOLLAR,
+          KC_4,    KC_5,    KC_6,    KC_QUOT,      KC_DQUO, KC_LBRC, KC_RBRC, ALGR(KC_SLSH), KC_QUES, KC_PIPE,
+          KC_7,    KC_8,    KC_9,    KC_COLN,      KC_SCLN, KC_LCBR, KC_RCBR, KC_LT,         KC_GT,   KC_BSLS,
+          _______, _______, _______, LCTL(KC_TAB), _______, _______ \
+          ),
+  [_RAISE] = LAYOUT(
+          KC_ESC,  KC_CIRC, KC_ASTR, KC_MINS, KC_UNDS, XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX, KC_DEL,
+          KC_CAPS, KC_TILD, KC_PERC, KC_PLUS, KC_EQL,  KC_LEFT,          KC_DOWN, KC_UP,   KC_RGHT, KC_AMPR,
+          XXXXXXX, KC_GRV,  XXXXXXX, XXXXXXX, XXXXXXX, ALGR(LSFT(KC_1)), KC_EXLM, XXXXXXX, XXXXXXX, TG(_MOUSE),
+          _______, _______, KC_TAB,  _______, _______, _______ \
+          ),
+  [_ADJUST] = LAYOUT(
+          KC_F1,   KC_F2,   KC_F3,       KC_F4,       KC_F5,   KC__MUTE, KC__VOLDOWN,   KC__VOLUP,   LCTL(LSFT(KC_SPC)), LCTL(LSFT(KC_V)),
+          KC_F6,   KC_F7,   KC_F8,       KC_F9,       KC_F10,  KC_HOME,  KC_PGDN,       KC_PGUP,     KC_END,             _______,
+          KC_F11,  KC_F12,  DF(_QWERTY), DF(_DVORAK), RESET,   _______,  LGUI(KC_DOWN), LGUI(KC_UP), LSFT(KC_PSCR),      KC_PSCR,
+          _______, _______, _______,     _______,     _______, _______ \
+          ),
   [_MOUSE] =  LAYOUT( \
-    XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  \
-    XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX,    XXXXXXX,    KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, XXXXXXX,  \
-    XXXXXXX, XXXXXXX, KC_MS_BTN1, KC_MS_BTN2, KC_MS_BTN3, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______,  \
-    XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX,    XXXXXXX,    _______ \
-    )
+          XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+          XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX,    XXXXXXX,    KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, XXXXXXX, \
+          XXXXXXX, XXXXXXX, KC_MS_BTN1, KC_MS_BTN2, KC_MS_BTN3, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______, \
+          XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX,    XXXXXXX,    _______ \
+          )
 };
 
 void persistant_default_layer_set(uint16_t default_layer) {
@@ -161,98 +63,13 @@ void persistant_default_layer_set(uint16_t default_layer) {
   default_layer_set(default_layer);
 }
 
-bool emacs_key(char *str, keyrecord_t *record) {
-    if (record->event.pressed) {
-        send_string(str);
-    }
-    return false;
+layer_state_t layer_state_set_user(layer_state_t state) {
+    return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
 }
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-    case LOWER:
-        if (record->event.pressed) {
-            layer_on(_LOWER);
-            update_tri_layer(_LOWER, _RAISE, _ADJUST);
-        } else {
-            layer_off(_LOWER);
-            update_tri_layer(_LOWER, _RAISE, _ADJUST);
-        }
-        return false;
-        break;
-    case RAISE:
-        if (record->event.pressed) {
-            layer_on(_RAISE);
-            update_tri_layer(_LOWER, _RAISE, _ADJUST);
-        } else {
-            layer_off(_RAISE);
-            update_tri_layer(_LOWER, _RAISE, _ADJUST);
-        }
-        return false;
-        break;
-    case EM_BUF_SELECT:
-        return emacs_key(SS_LCTRL("x")"b", record);
-        break;
-    case EM_BUF_OTHER:
-        return emacs_key(SS_LCTRL("x")"o", record);
-        break;
-    case EM_BUF_HIDE:
-        return emacs_key(SS_LCTRL("x")"0", record);
-        break;
-    case EM_BUF_KILL:
-        return emacs_key(SS_LCTRL("x")"k"SS_TAP(X_ENTER), record);
-        break;
-    case EM_SPLIT_NONE:
-        return emacs_key(SS_LCTRL("x")"1", record);
-        break;
-    case EM_SPLIT_V:
-        return emacs_key(SS_LCTRL("x")"3", record);
-        break;
-    case EM_SPLIT_H:
-        return emacs_key(SS_LCTRL("x")"2", record);
-        break;
-    case EM_ORG_CL_I:
-        return emacs_key(SS_DOWN(X_LCTRL)"cxi"SS_UP(X_LCTRL), record);
-        break;
-    case EM_ORG_CL_O:
-        return emacs_key(SS_DOWN(X_LCTRL)"cxo"SS_UP(X_LCTRL), record);
-        break;
-    case EM_COMMENT:
-        return emacs_key(SS_DOWN(X_LCTRL)"x;"SS_UP(X_LCTRL), record);
-        break;
-    case EM_TOP:
-        return emacs_key(SS_LALT("<"), record);
-        break;
-    case EM_BOTTOM:
-        return emacs_key(SS_LALT(">"), record);
-        break;
-    case EM_W_INC_H:
-        return emacs_key(SS_LCTRL("x")"}", record);
-        break;
-    case EM_W_INC_V:
-        return emacs_key(SS_LCTRL("x")"^", record);
-        break;
-    case EM_REG_SET:
-        return emacs_key(SS_LCTRL("x")"r 1", record);
-        break;
-    case EM_REG_JUMP:
-        return emacs_key(SS_LCTRL("x")"rj1", record);
-        break;
-    case EM_MAC_ST:
-        return emacs_key(SS_LCTRL("x")"(", record);
-        break;
-    case EM_MAC_END:
-        return emacs_key(SS_LCTRL("x")")", record);
-        break;
-    case EM_QUERY_R:
-        return emacs_key(SS_LALT("%"), record);
-        break;
-    case EM_REGEX_R:
-        return emacs_key(SS_LCTRL(SS_LALT("%")), record);
-        break;
-    case EM_ZAP:
-        return emacs_key(SS_LALT("z"), record);
-        break;
-    }
-    return true;
+bool custom_key(keyrecord_t *record, const char *seq) {
+  if (record->event.pressed) {
+    send_string(seq);
+  }
+  return false;
 }
